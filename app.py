3ff07d1d5d1700ce48617ecc16b478eff74739bd
Ashish-Adidas <97599701+Ashish-Adidas@users.noreply.github.com>
2023-10-06 04:54:34 +0000

    streamlit change

--- app.py
+++ app.py
@@ -3,9 +3,19 @@
 # Licensed under the MIT License. See LICENSE in the project root for license information.
 #-----------------------------------------------------------------------------------------
 
-from flask import Flask
-app = Flask(__name__)
+import streamlit as st
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+import os
+
+# Get the path to the static folder
+static_folder = os.path.join(os.path.dirname(__file__), "static")
+
+# Specify the relative path to the index.html file
+index_html_path = os.path.join(static_folder, "index.html")
+
+# Load the index.html file
+with open(index_html_path, "r") as file:
+    index_html = file.read()
+
+# Display the index.html file in the Streamlit app
+st.markdown(index_html, unsafe_allow_html=True)
